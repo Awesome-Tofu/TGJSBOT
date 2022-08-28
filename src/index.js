bca4632d658387eb52428f49634d09815489b6de
Tofu <91769994+Awesome-Tofu@users.noreply.github.com>
2022-08-28 15:05:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-// const Promise = require('node-telegram-bot-api');
-// Promise.config({
-//   cancellation: true
-// });
 const telegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const chatbot = require("node-fetch").default;
 
 const TOKEN = process.env.BOT_TOKEN;
 
-const bot = new telegramBot(TOKEN, {polling: true});
+const bot = new telegramBot(TOKEN, { polling: true });
 
 bot.on('message', (message) => {
-    let chat_id = message.from.id;
-    //middle dg
+  let chat_id = message.from.id;
+  var sticker = message.sticker.file_id;
+  var unique_sticker = message.sticker.file_unique_id;
+  var isanimated = message.sticker.is_animated;
+  var isvideo = message.sticker.is_video;
+  var emoji = message.sticker.emoji;
+  
+  console.log(message);
+  
+  bot.sendMessage(chat_id, "Sticker ID: " + sticker);
+  bot.sendMessage(chat_id, "Unique Sticker ID: " + unique_sticker);
+  bot.sendMessage(chat_id, "Is Animated: " + isanimated);
+  bot.sendMessage(chat_id, "Is Video: " + isvideo);
+  bot.sendMessage(chat_id, "Emoji: " + emoji);
 
-    bot.sendMessage(chat_id, 'HELLO WORLD!');
 });
