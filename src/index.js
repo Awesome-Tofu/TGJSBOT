da2575db4d38207af874779427147cca672e8729
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-01-28 19:09:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ const helpMessage= `
 Say something to me and I will generate image according to your text
 To generate image, use 
 /gene <query> 
+/tip tips for generating image according to you
 `
 
 bot.onText(/\/start/, (msg) => {
-    let chat_id = msg.from.id;
+    let chat_id = msg.chat.id;
     console.log();
     bot.sendMessage(chat_id,`Hii ${msg.chat.first_name+" "+msg.chat.last_name}, I am <a href=\"https://t.me/Aki_Image_UltraBot\">ᴀᴋɪ ʜᴀʏᴀᴋᴀᴡᴀ</a><a href=\"https://graph.org/file/06f943b53be73a0d3a8bc.png\">,</a> who can generate image.\nI am based on <a href=\"https://openai.com/dall-e-2/\">DALL-E API.</a>`,{parse_mode : "HTML"})
   
@@ -22,13 +23,14 @@ bot.onText(/\/start/, (msg) => {
 
 
 bot.onText(/\/help/,(msg)=>{
-    bot.sendMessage(msg.from.id,helpMessage);
+    let chat_id=msg.chat.id;
+    bot.sendMessage(chat_id,helpMessage);
 })
 
 
 
 bot.onText(/\/gene/, (msg) => {
-     let chat_id = msg.from.id;
+     let chat_id = msg.chat.id;
     const inputmsg= msg.text;
   let inputArray = inputmsg.split(" ");
   let message="";
@@ -41,7 +43,7 @@ bot.onText(/\/gene/, (msg) => {
 
 
 bot.onText(/\/gene/, (msg) => {
-  let chat_id = msg.from.id;
+  let chat_id = msg.chat.id;
   const inputmsg= msg.text;
   let inputArray = inputmsg.split(" ");
   let message="";
@@ -73,17 +75,17 @@ bot.onText(/\/gene/, (msg) => {
 });
 
 bot.onText(/\/testpic/, (msg) => {
-  let chat_id = msg.from.id;
+  let chat_id = msg.chat.id;
     const url = 'https://graph.org/file/68adc7cdedc76f8d59aa9.jpg';
     bot.sendPhoto(chat_id, url,{caption:"Test Image"});
 })
 
 bot.onText(/\/tip/,(msg)=>{
-  let chat_id = msg.from.id;
+  let chat_id = msg.chat.id;
     bot.sendMessage(chat_id,"<a href=\"https://pitch.com/v/tmd33y/d959fd01-3eea-4b16-9472-e79ccb635e98/\">Here are some Tips</a>",{parse_mode : "HTML"})
 })
 
 bot.onText(/\/owner/,(msg)=>{
-    bot.sendMessage(msg.from.id,"my owner is <a href=\"t.me/awesome_tofu\">𝗧𝗢𝗙𝗨鬼</a>. Thanks for using me!",{parse_mode : "HTML"})
+    bot.sendMessage(msg.chat.id,"my owner is <a href=\"t.me/awesome_tofu\">𝗧𝗢𝗙𝗨鬼</a>. Thanks for using me!",{parse_mode : "HTML"})
 })
 
