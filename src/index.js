ca480767baf6805cc0115eff8435039351e7b62f
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-01-29 21:42:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@
 const telegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 const {Configuration, OpenAIApi} = require('openai')
-
+const OWNER_ID= process.env.OWNER_ID;
+const OWNER_NAME= process.env.OWNER_NAME;
 const TOKEN = process.env.BOT_TOKEN;
 
 const bot = new telegramBot(TOKEN, { polling: true });
 
 const helpMessage= `
 Say something to me and I will generate image according to your text
-To generate image, use 
-/gene <query> 
-/tip tips for generating image according to you
+
+/gene <query> | generate images.
+/tip tips for generating image according to you.
+/ask <query> | ask me anything.
+/report <report query> | enter report query to my owner.
+/repository | My repo on github.
 `
 
 bot.onText(/\/start/, (msg) => {
     let chat_id = msg.chat.id;
     console.log();
-    bot.sendMessage(chat_id,`Hii ${msg.chat.first_name+" "+msg.chat.last_name}, I am <a href=\"https://t.me/Aki_Image_UltraBot\">ᴀᴋɪ ʜᴀʏᴀᴋᴀᴡᴀ</a><a href=\"https://graph.org/file/06f943b53be73a0d3a8bc.png\">,</a> who can generate image.\nI am based on <a href=\"https://openai.com/dall-e-2/\">DALL-E API.</a>`,{parse_mode : "HTML"})
+    bot.sendMessage(chat_id,`Hii ${msg.from.first_name+" "+msg.from.last_name}, I am <a href=\"https://t.me/Aki_Image_UltraBot\">ᴀᴋɪ ʜᴀʏᴀᴋᴀᴡᴀ</a><a href=\"https://graph.org/file/06f943b53be73a0d3a8bc.png\">,</a> who can generate image.\nI am based on <a href=\"https://openai.com/dall-e-2/\">DALL-E API.</a>`,{parse_mode : "HTML"})
   
   });
 
@@ -26,6 +30,7 @@ bot.onText(/\/help/,(msg)=>{
     let chat_id=msg.chat.id;
     bot.sendMessage(chat_id,helpMessage);
 })
+
 
 
 
@@ -57,7 +62,7 @@ bot.onText(/\/gene/, (msg) => {
       const config = new Configuration({
         apiKey: apikey
     });
-    const openai = new OpenAIApi(config);
+    let openai = new OpenAIApi(config);
     const response = openai.createImage({
         prompt: message,
         n: 1,
@@ -86,6 +91,93 @@ bot.onText(/\/tip/,(msg)=>{
 })
 
 bot.onText(/\/owner/,(msg)=>{
-    bot.sendMessage(msg.chat.id,"my owner is <a href=\"t.me/awesome_tofu\">𝗧𝗢𝗙𝗨鬼</a>. Thanks for using me!",{parse_mode : "HTML"})
+    bot.sendMessage(msg.chat.id,`my owner is <a href=\"t.me/awesome_tofu\">OWNER_NAME</a>. Thanks for using me!`,{parse_mode : "HTML"})
 })
 
+
+
+bot.onText(/\/ask/, (msg) => {
+
+ const inputmsg= msg.text;
+  let inputArray = inputmsg.split(" ");
+  let message="";
+  if(inputArray.length==1){
+    bot.sendMessage(msg.chat.id,"Please input the text/query!");
+  }else{
+inputArray.shift();
+     message=inputArray.join(" ");
+
+  BOT_NAME="Aki"
+  const prompt_template =
+    "I am Telegram AI Bot" +
+    BOT_NAME +
+    "Developed by telegram.me/Awesome_tofu \n human: How are you? \n" +
+    BOT_NAME +
+    "I am well, how are can I can you? \n human:" +
+    message +
+    "\n" +
+    BOT_NAME +
+    ":";
+  
+  const configu = new Configuration({
+    apiKey: process.env.API
+});
+let openai = new OpenAIApi(configu);
+
+    const resp = openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt_template,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    resp.then((data) => {
+      const message = data.data.choices[0].text;
+        console.log("@"+msg.from.username+": "+inputArray.join(" "));
+      bot.sendMessage(msg.chat.id,message);
+  }).catch((err) => {
+      bot.sendMessage(msg.chat.id,"ERROR!!")
+  })
+
+  }
+  
+
+})
+
+
+
+bot.onText(/\/report/, (msg) => {
+    function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function myFunction() {
+  bot.sendMessage(msg.chat.id,"<code>Reporting...</code>",{parse_mode:"HTML"})
+  await sleep(1500);
+  bot.deleteMessage(msg.chat.id,msg.message_id+1);
+  bot.sendMessage(msg.chat.id,"<code>Reported!</code>",{parse_mode:"HTML"})
+}
+
+myFunction();
+    bot.sendMessage(OWNER_ID,`from: @${msg.from.username},[<code>${msg.from.id}</code>]
+    ${msg.text}`,{parse_mode : "HTML"});
+    // console.log(msg.message_id+1)
+    
+    
+})
+
+bot.onText(/\/repository/, (msg) => {
+    bot.sendMessage(msg.chat.id,"<a href=\"github.com/Awesome-tofu/TGJSBOT\">Github Repo.</a>",{parse_mode : "HTML"})
+})
+
+
+
+
+
+bot.on('polling_error', (error) => {
+  console.log(error.code);  // => 'EFATAL'
+});
+        
